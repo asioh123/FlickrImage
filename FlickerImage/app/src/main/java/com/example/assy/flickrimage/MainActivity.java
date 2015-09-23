3c101cd6a150f78f778968d77230d5bc26873fed
asioh123 <asi-o@info.cts.org.il>
2015-09-23 16:14:59 +0300

    start2

---
+++ FlickerImage/app/src/main/java/com/example/assy/flickrimage/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.assy.flickrimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mainListView ;
+    private ArrayAdapter<String> listAdapter ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainListView = (ListView) findViewById( R.id.mainListView );
+
+        // Create and populate a List of planet names.
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+                "Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll( Arrays.asList(planets) );
+
+        // Create ArrayAdapter using the planet list.
+        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+
+        // Add more planets. If you passed a String[] instead of a List<String>
+        // into the ArrayAdapter constructor, you must not add more items.
+        // Otherwise an exception will occur.
+        listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add("Eris");
+        listAdapter.add( "Eris" );
+        listAdapter.add( "Eris" );
+        listAdapter.add( "Eris" );
+        listAdapter.add( "Eris" );
+        listAdapter.add( "Eris" );
+        listAdapter.add( "Eris" );
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
